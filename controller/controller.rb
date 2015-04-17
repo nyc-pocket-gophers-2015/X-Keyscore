284db26070050905e3ae4ba3e1944d324d84058b
fishermanng & Tanya Trinkle <pair+fishermanng.ttrinkle@devbootcamp.com>
2015-04-17 16:52:29 -0400

    add quit and scoring system

--- controller/controller.rb
+++ controller/controller.rb
@@ -10,6 +10,7 @@ attr_reader :deck
   def initialize(deck)
     @deck = deck
     @card_index = 0
+    @score = 0
   end
 
   def user_input
@@ -23,6 +24,10 @@ attr_reader :deck
       View.end_game
       return
     elsif player_start == "1"
+      View.deck_order
+      if user_input == 'yes'
+        deck.shuffle
+      end
       start
     else
       View.invalid_input
@@ -32,42 +37,61 @@ attr_reader :deck
 
   def start
     # Sets a current card equal to a "drawn" card object
-    @current_card = deck.draw_card(@card_index)
-    # Displays a card object
-    View.display(@current_card.question)
-    guess
+    sleep (1.2)
+    system "clear"
+    if @card_index == deck.length
+      View.play_again
+      user_input
+      if user_input == "yes"
+        @card_index = 0
+        start
+      else
+        View.end_game
+        View.score(@score)
+      end
+    else
+      @current_card = deck.draw_card(@card_index)
+      # Displays a card object
+      View.display(@current_card.question)
+      guess
+    end
   end
 
   def guess
-    guess_counter = 1
-    player_guess = user_input
-    if player_guess == @current_card.answer
-      View.correct_answer(player_guess)
-      @card_index = @card_index + 1
-      start
-    else
-      View.wrong_answer(player_guess)
-      guess_counter += 1
-      View.hint1(@current_card.answer)
+    until user_input == 'quit'
+      guess_counter = 1
       player_guess = user_input
       if player_guess == @current_card.answer
         View.correct_answer(player_guess)
-        @card_index += 1
+        @card_index = @card_index + 1
+        @score += 3
         start
       else
         View.wrong_answer(player_guess)
         guess_counter += 1
-        View.hint2(@current_card.answer)
+        View.hint1(@current_card.answer)
         player_guess = user_input
         if player_guess == @current_card.answer
           View.correct_answer(player_guess)
+          @score += 2
           @card_index += 1
           start
         else
           View.wrong_answer(player_guess)
-          View.correct_answer(@current_card.answer)
-          @card_index += 1
-          start
+          guess_counter += 1
+          View.hint2(@current_card.answer)
+          player_guess = user_input
+          if player_guess == @current_card.answer
+            View.correct_answer(player_guess)
+            @score += 1
+            @card_index += 1
+            start
+          else
+            View.wrong_answer(player_guess)
+            View.correct_answer(@current_card.answer)
+            @card_index += 1
+            start
+          end
         end
       end
     end
